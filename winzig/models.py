e3fd9c980d4ad602f8aa2b004ef151f2480d46a7
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-03-11 19:09:39 +0100

    feat(models): add index to Post url field

--- winzig/models.py
+++ winzig/models.py
@@ -4,5 +4,5 @@ from sqlmodel import Field, SQLModel
 
 class Post(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    url: str
+    url: str = Field(index=True)
     content: str
