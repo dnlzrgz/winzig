ccaf5ee1ad746573172855f9c62b91c4cb11ff6c
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-03-27 07:48:46 +0100

    refactor(crawl): migrate from httpx to aiohttp

--- winzig/crawler.py
+++ winzig/crawler.py
@@ -1,7 +1,7 @@
 import asyncio
 from collections import Counter
 from itertools import batched
-import httpx
+import aiohttp
 import feedparser
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -18,25 +18,22 @@ headers = {
 }
 
 
-async def fetch_content(client: httpx.AsyncClient, url: str) -> bytes | None:
+async def fetch_content(client: aiohttp.ClientSession, url: str) -> str | None:
     try:
-        async with client.stream("GET", url) as response:
-            if response.status_code >= 400:
+        async with client.get(url) as resp:
+            if resp.status >= 400:
                 console.log(
-                    f"[red bold]Error[/red bold]: Bad status from '{url}': {response.status_code}"
+                    f"[red bold]Error[/red bold]: Bad status from '{url}': {resp.status}"
                 )
                 return None
 
-            return await response.aread()
-    except httpx.HTTPError:
-        console.log(f"[red bold]Error[/red bold]: Got HTTP error from '{url}'")
-        return None
-    except ValueError as e:
+            return await resp.text()
+    except aiohttp.ClientError as e:
         console.log(f"[red bold]Error[/red bold]: Failed to fetch '{url}': {e}")
         return None
 
 
-def clean_content(html: bytes) -> str:
+def clean_content(html: str) -> str:
     tree = HTMLParser(html)
     for tag in tree.css(
         "script, style, link, noscript, object, img, embed, iframe, svg, canvas, form, audio, video"
@@ -60,11 +57,11 @@ def is_feed(url: str) -> bool:
 
 
 async def get_posts_from_feed(
-    session: AsyncSession, feed_id: int, url: str, max: int | None
+    session: AsyncSession, feed: Feed, max: int | None
 ) -> list[str]:
     try:
-        feed = feedparser.parse(url)
-        statement = select(Post).where(Post.feed_id == feed_id)
+        d = feedparser.parse(feed.url)
+        statement = select(Post).where(Post.feed_id == feed.id)
         results = await session.execute(statement)
         results = results.scalars()
         posts_db_urls = {post.url for post in results}
@@ -72,21 +69,21 @@ async def get_posts_from_feed(
         if max:
             return [
                 entry.link
-                for entry in feed.entries[:max]
+                for entry in d.entries[:max]
                 if entry.link not in posts_db_urls
             ]
         else:
             return [
-                entry.link for entry in feed.entries if entry.link not in posts_db_urls
+                entry.link for entry in d.entries if entry.link not in posts_db_urls
             ]
     except Exception as e:
-        console.log(f"[red bold]Error[/red bold]: Parsing feed '{url}': {e}")
+        console.log(f"[red bold]Error[/red bold]: Parsing feed '{feed.url}': {e}")
         return []
 
 
 async def process_post(
     session: AsyncSession,
-    client: httpx.AsyncClient,
+    client: aiohttp.ClientSession,
     feed: Feed | None,
     url: str,
 ) -> None:
@@ -167,7 +164,7 @@ async def crawl_links(session: AsyncSession, urls: list[str], batch_size: int = 
         if not post:
             post_urls.append(url)
 
-    async with httpx.AsyncClient(headers=headers, follow_redirects=True) as client:
+    async with aiohttp.ClientSession(headers=headers) as client:
         with console.status("Fetching posts...", spinner="earth") as status:
             for batch in batched(post_urls, batch_size):
                 tasks = [process_post(session, client, None, url) for url in batch]
@@ -193,10 +190,10 @@ async def crawl_from_feeds(
         console.log("[red]Error[/red]: No feeds found!")
         return
 
-    async with httpx.AsyncClient(headers=headers, follow_redirects=True) as client:
+    async with aiohttp.ClientSession(headers=headers) as client:
         with console.status("Fetching posts...", spinner="earth") as status:
             for idx, feed in enumerate(feeds):
-                posts = await get_posts_from_feed(session, feed.id, feed.url, max)
+                posts = await get_posts_from_feed(session, feed, max)
                 tasks = [process_post(session, client, feed, post) for post in posts]
 
                 status.update(
