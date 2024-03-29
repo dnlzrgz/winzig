3ebf4b7c9620174ec5d7393cda437da808497625
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-03-29 16:06:20 +0100

    refactor: refactor a bit some util functions

--- winzig/utils.py
+++ winzig/utils.py
@@ -2,18 +2,14 @@ import string
 import emoji
 
 emojis = set(emoji.EMOJI_DATA.keys())
+punctuation_and_emojis = set(string.punctuation + "“”’‘¶■▌▲▼└│─√©" + "".join(emojis))
 
-translation_table = str.maketrans(
-    {char: " " for char in string.punctuation + "“”’‘¶■▌▲▼└│─√©" + "".join(emojis)}
-)
+translation_table = str.maketrans(({char: " " for char in punctuation_and_emojis}))
 
 
 def update_url_scores(old: dict[str, float], new: dict[str, float]) -> dict[str, float]:
     for url, score in new.items():
-        if url in old:
-            old[url] += score
-        else:
-            old[url] = score
+        old[url] = old.get(url, 0.0) + score
 
     return old
 
@@ -26,6 +22,5 @@ def normalize_text(text: str) -> str:
 
 def get_top_urls(scores_dict: dict, n: int):
     sorted_urls = sorted(scores_dict.items(), key=lambda x: x[1], reverse=True)
-    top_n_urls = sorted_urls[:n]
-    top_n_dict = dict(top_n_urls)
+    top_n_dict = dict(sorted_urls[:n])
     return top_n_dict
